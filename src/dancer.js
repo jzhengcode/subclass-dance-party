340cd7efe9c2c6388488e6e77cc1bbba3dab7395
Janet Zheng <janet.zhengg@gmail.com>
2020-05-18 19:32:39 -0700

    refractored dancer and blinky dancer to pseudoclassical instantiation

--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,49 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps) {
+var MakeDancer = function(verPos, horiPos, timeBetweenSteps) {
 
-  var dancer = {};
+  Object.create(MakeDancer.prototype);
+
+  this.vertLoc = verPos;
+  this.horiLoc = horiPos;
+  this.timeBetweenSteps = timeBetweenSteps;
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
 
-  dancer.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
+  // dancer.step = function() {
+  //   // the basic dancer doesn't do anything interesting at all on each step,
+  //   // it just schedules the next step
+  //   setTimeout(dancer.step, timeBetweenSteps);
+  // };
+  this.step();
 
-  dancer.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
+  // dancer.setPosition = function(top, left) {
+  //   // Use css top and left properties to position our <span> tag
+  //   // where it belongs on the page. See http://api.jquery.com/css/
+  //   //
+  //   var styleSettings = {
+  //     top: top,
+  //     left: left
+  //   };
+  //   dancer.$node.css(styleSettings);
+  // };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  this.setPosition(verPos, horiPos);
 
-  return dancer;
+  // return dancer;
 };
+
+MakeDancer.prototype.step = function() {
+  setTimeout(this.step, this.timeBetweenSteps)
+};
+
+MakeDancer.prototype.setPosition = function(verPos, horiPos){
+  var styleSettings = {
+    top: verPos,
+    left: horiPos,
+  };
+
+  this.$node.css(styleSettings);
+}
